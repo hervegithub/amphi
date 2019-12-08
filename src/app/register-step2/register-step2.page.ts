0fb3e492ca63a3940f09909c34dca4632c198357
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2019-12-08 17:11:19 +0100

    Creation register-step2

---
+++ src/app/register-step2/register-step2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-step2',
+  templateUrl: './register-step2.page.html',
+  styleUrls: ['./register-step2.page.scss'],
+})
+export class RegisterStep2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
