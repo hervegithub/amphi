5749baa91be085c149bb5fc24d2894edffc7fd87
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2019-12-09 01:04:38 +0100

    liste des teachers

--- src/app/home/teachers-list/teachers-list.page.ts
+++ src/app/home/teachers-list/teachers-list.page.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeachersListPage implements OnInit {
 
-  constructor() { }
+  listes= [];
+  constructor() {
+    this.listes = 
+    [
+      {
+        nom:"Mark Angel",
+        ville:"Douala",
+        niveau:"Master2",
+        recommandation: 2,
+        etoile:2,
+      },
+      {
+        nom:"Mark Angel",
+        ville:"Douala",
+        niveau:"Master2",
+        recommandation: 2,
+        etoile:2,
+      },
+      {
+        nom:"Mark Angel",
+        ville:"Douala",
+        niveau:"Master2",
+        recommandation: 2,
+        etoile:2,
+      }
+    ]
+   }
 
   ngOnInit() {
   }
